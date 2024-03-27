ace4721c3c98b9282f6fa1ed6f0f10f5199dbd06
ahmed-samir <ahmedsamirr126@gmail.com>
2024-03-27 15:08:52 +0200

    working on js file

--- main.js
+++ main.js
@@ -1,10 +1,24 @@
 
 let num = document.querySelectorAll(".num li")
+let numArr = Array.from(num)
+
+let btn = document.querySelector(".btn")
+
+let rat = document.querySelector(".rat")
+console.log(rat);
+
 console.log(num);
-num.forEach((ele) =>{
-  ele.addEventListener("click", function(ele) {
-    ele.target.classList.toggle("active")
+numArr.forEach((li) =>{
+  li.addEventListener("click", function(ele) {
+    numArr.forEach(ele => {
+      ele.classList.remove("active")
+    });
+    ele.target.classList.add("active")
   })
+})
+
+btn.addEventListener("click", (e) =>{
+  rat.style.display = "block"
 })
 
 
